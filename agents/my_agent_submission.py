ce4e23971a1b92bdee1598c50bd43d80eb474ca1
piergiuseppepezzoli <pezzoli.piergiuseppe@gmail.com>
2025-05-06 16:48:17 +0200

    Change default agent

--- agents/my_agent_submission.py
+++ agents/my_agent_submission.py
@@ -8,8 +8,8 @@ class ChangeMyNameAgent(Agent):
     def get_hint(self, taboo_list: list[str], guess_word: str, level: int) -> str:
         ### YOUR CODE HERE ###
         # You can call chatgpt like this:
-        # hint = self.llm.generate_answer(prompt="what is the answer to life the universe and everything?")
-        hint = "42"  # Example answer
+        hint = self.llm.generate_answer(prompt="what is the answer to life the universe and everything?")
+        # hint = "42"  # Example answer
         ### YOUR CODE HERE ###
 
         return hint
